c17f83913c8c4bdf6f7f090fe9f17dd888b95edd
Michael Eisel <michael.eisel@gmail.com>
2020-03-19 13:51:40 -0400

    commit

--- Sources/PathKitCExt/include/PATPathKit.h
+++ Sources/PathKitCExt/include/PATPathKit.h
@@ -1,3 +1,13 @@
 
+#include <stddef.h>
+
+#ifdef __cplusplus
+export "C" {
+#endif
+
 void PATFreePathComponents(const char **components, void *temp);
 const char **PATPathComponents(const char *path, size_t *count, void **temp);
+
+#ifdef __cplusplus
+}
+#endif
