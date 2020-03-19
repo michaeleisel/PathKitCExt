13f8074b9cc83e9b5b6fb000d117867d41c56b22
Michael Eisel <michael.eisel@gmail.com>
2020-03-19 13:48:51 -0400

    update

--- Sources/PathKitCExt/PATPathKit.cpp
+++ Sources/PathKitCExt/PATPathKit.cpp
@@ -1,5 +1,5 @@
 #include "PATPathKit.h"
-#include <cstring>
+#include <string>
 #include <vector>
 #include <string>
 
---
+++ Sources/PathKitCExt/include/PATPathKit.h
@@ -0,0 +1,3 @@
+
+void PATFreePathComponents(const char **components, void *temp);
+const char **PATPathComponents(const char *path, size_t *count, void **temp);
